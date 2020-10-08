fa97523c6a6054810dd8ae220e19ab24b310095c
Vdovenkova <ivdovenkova@mail.ru>
2020-10-08 16:26:13 +0300

    Сохраняю новые файлы

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let money;
+
+let income;
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period;
+
+alert("Вывод любого текста в модальном окне");
+
+console.log("Вывод текста в консоль инструмента разработчика в браузере");
