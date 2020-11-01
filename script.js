e47167a080c28722068aad1c8f8eff50e94a0c7d
Vdovenkova <ivdovenkova@mail.ru>
2020-11-01 19:57:00 +0300

    16-2 - вариант 2

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const myLesson = [
+  {lesson: 1, type: 'basic', points: 2},
+  {lesson: 2, type: 'additional', points: 4},
+  {lesson: 3, type: 'basic', points: 6},
+  {lesson: 4, type: 'additional', points: 3},
+  {lesson: 5, type: 'basic', points: 4},
+  {lesson: 6, type: 'basic', points: 2},
+  {lesson: 7, type: 'additional', points: 2},
+  {lesson: 8, type: 'basic', points: 6},
+  {lesson: 9, type: 'basic', points: 4},
+  {lesson: 10, type: 'basic', points: 6},
+  {lesson: 11, type: 'additional', points: 5}, 
+  {lesson: 12, type: 'basic', points: 2}, 
+  {lesson: 13, type: 'additional', points: 2}, 
+  {lesson: 14, type: 'basic', points: 4},
+  {lesson: 15, type: 'additional', points: 1},
+  {lesson: 16, type: 'additional', points: 7}
+];
+
+myLesson.forEach((item) => {
+  if (item.type === 'basic') {
+    item.points = item.points / 2;
+  } else if (item.type === 'additional') {
+    myLesson.splice(myLesson.indexOf(item), 1);
+    // console.log('lesson', item.lesson);
+  }
+});
+
+console.log(myLesson);
