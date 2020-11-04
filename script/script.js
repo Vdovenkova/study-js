f36525735db391712d343463c628e80dff189ee7
Vdovenkova <ivdovenkova@mail.ru>
2020-11-05 01:24:19 +0300

    18 - таймер - готово

--- script/script.js
+++ script/script.js
@@ -3,12 +3,13 @@ window.addEventListener('DOMContentLoaded', function(){
 
   // timer
   function countTimer(deadline){
-    let timerHours = document.querySelector('#timer-hours'),
+    const timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
+        // count = 0;
 
     function getTimeRemaining(){
-      let dateStop = new Date(deadline).getTime(),
+      const dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
           // в секундах сколько осталось до даты Х
           timeRemaining = (dateStop - dateNow) / 1000,
@@ -19,41 +20,36 @@ window.addEventListener('DOMContentLoaded', function(){
       return {timeRemaining, hours, minutes, seconds};
     }
 
-    function updateClock(){
-      let timer = getTimeRemaining();
+    const idUpdateClock = setInterval(function() {
+      // count++;
+      const timer = getTimeRemaining();
 
-      if (timer.timeRemaining <= 0) {
-        timerHours.textContent = '00';
-      } else if (timer.hours < 10) {
-        timerHours.textContent = `0${timer.hours}` ;
+      if (timer.hours < 10 && timer.hours >= 0) {
+        timerHours.textContent = `0${timer.hours}`;
       } else {
         timerHours.textContent = timer.hours;
       }
-      
-      if (timer.timeRemaining <= 0) {
-        timerMinutes.textContent = '00';
-      } else if (timer.sinutes < 10) {
-        timerMinutes.textContent = `0${timer.minutes}` ;
+
+      if (timer.minutes < 10 && timer.minutes >= 0) {
+        timerMinutes.textContent = `0${timer.minutes}`;
       } else {
         timerMinutes.textContent = timer.minutes;
       }
 
-      if (timer.timeRemaining <= 0) {
-        timerSeconds.textContent = '00';
-      } else if (timer.seconds < 10) {
-        timerSeconds.textContent = `0${timer.seconds}` ;
+      if (timer.seconds < 10 && timer.seconds >= 0) {
+        timerSeconds.textContent = `0${timer.seconds}`;
       } else {
         timerSeconds.textContent = timer.seconds;
       }
-      
-      if (timer.timeRemaining > 0) {
-        setTimeout(updateClock, 1000);
+
+      if (timer.timeRemaining < 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(idUpdateClock);
       }
-    }
-
-    updateClock();
-
+      // console.log(count);
+    }, 1000);
   }
-
   countTimer('7 november 2020');
 });
