7b9871182bc8f22034756bf969ea378b9d04d58e
Vdovenkova <ivdovenkova@mail.ru>
2020-11-10 13:42:21 +0300

    20й - табы - готово

--- script/script.js
+++ script/script.js
@@ -142,4 +142,34 @@ window.addEventListener('DOMContentLoaded', function(){
   }
   slowScrollService();
 
+  // табы
+  const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'), //весь блок
+          tab = tabHeader.querySelectorAll('.service-header-tab'), //заголовки
+          tabContent = document.querySelectorAll('.service-tab'); //содержимое
+    
+    const toggleTabContent = (index) => {
+      for (let ind = 0; ind < tabContent.length; ind++){
+        if (index === ind){
+          tab[ind].classList.add('active');
+          tabContent[ind].classList.remove('d-none');
+        } else {
+          tab[ind].classList.remove('active');
+          tabContent[ind].classList.add('d-none');
+        }
+      }
+    };
+
+    tabHeader.addEventListener('click', (event) => {
+      let target = event.target;
+      target = target.closest('.service-header-tab');
+
+      if(target){
+        tab.forEach((item, ind) => {
+          if(item === target) {toggleTabContent(ind);}
+        });
+      }
+    });
+  };
+  tabs();
 });
