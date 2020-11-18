50b85bef14b063367e61e036709572a6f3737d05
Vdovenkova <ivdovenkova@mail.ru>
2020-11-18 16:56:00 +0300

    30й часть1 - изменила дату для таймера

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 
 // таймер
-countTimer('18 november 2020');
+countTimer('25 november 2020');
 // плавная прокрутка к блоку сервис
 slowScrollService();
 // меню
