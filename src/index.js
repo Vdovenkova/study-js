1920f1c39ddb38911b35e176ae10af9e1a4952ad
Vdovenkova <ivdovenkova@mail.ru>
2020-11-22 22:55:37 +0300

    32 - проба карусели

--- src/index.js
+++ src/index.js
@@ -15,6 +15,28 @@ import changeImg from './modules/changeImg';
 import inputNum from './modules/inputNum';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
+import SliderCarousel from './modules/sliderCarousel';
+// слайдер-карусель
+const carousel = new SliderCarousel({
+  main: '.companies-wrapper',
+  wrap: '.companies-hor',
+  slidesToShow: 4,
+  infinity: true,
+  responsive: [{
+      breakpoint: 1024,
+      slidesToShow: 3
+    },
+    {
+      breakpoint: 768,
+      slidesToShow: 2
+    },
+    {
+      breakpoint: 576,
+      slidesToShow: 1
+    }
+  ]
+});
+carousel.init();
 
 // таймер
 countTimer('25 november 2020');
